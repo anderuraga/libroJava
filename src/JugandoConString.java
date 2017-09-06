b29cfb9974388e1a0bb6a7a4b98179fd9d48e84f
anderuraga <ander.uraga@gmail.com>
2017-09-06 10:28:18 +0200

    planteado ejercicio

---
+++ src/JugandoConString.java
@@ -0,0 +1,46 @@
+
+public class JugandoConString {
+
+	public static void main(String[] args) {
+		
+		String manolo = "Manolo Kabezabolo";
+		
+		//convertir a minusculas
+		
+		//convertir a mayusculas
+		
+		//mostrar numero de caracteres
+		
+		//mostrar posiscion de la letra "K"
+		
+		
+		//separar en dos variables el nombre y ´apellido' con substring
+		String nombre; //Manolo
+		String apellido; // Kabezabolo
+		
+		
+		/*   Comparando   */
+		
+		String c1 = "casa";
+		String c2 = "Casa";
+		
+		//comparar c1 y c2 con equals y equalIgnoreCase
+		
+		
+		/* Limpiando entradas de datos */
+		
+		String c3 = "perro";
+		String c4 = "     perro     ";
+		
+		//las dos variables deden ser igual => trim
+		
+		
+		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique magna non elementum facilisis. Suspendisse ultricies finibus massa, at ullamcorper felis aliquet a. Aliquam gravida volutpat purus nec faucibus. Ut sodales enim ac gravida fermentum. Aenean id est at mauris vehicula tincidunt et sit amet erat. Morbi et tincidunt massa, scelerisque maximus quam. Sed hendrerit fermentum venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin faucibus hendrerit sem. Aliquam erat volutpat. Ut in condimentum eros. Praesent molestie justo quis porta finibus. Etiam laoreet, odio at auctor sodales, arcu arcu finibus ligula, a facilisis elit tellus laoreet turpis. Nulla mattis mattis mi sed aliquet. Duis dignissim commodo vulputate.";
+		//mostrar longitud
+		//contar todas las vocales
+		//indicar el numero de veces que aparece 'at'
+		
+		
+	}
+	
+}
