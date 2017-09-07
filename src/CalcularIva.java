54799eb2c43017692dd51d50a0bcdf924357e5e9
anderuraga <ander.uraga@gmail.com>
2017-09-07 09:06:31 +0200

    solucionado ejercicio Iva

--- src/CalcularIva.java
+++ src/CalcularIva.java
@@ -4,17 +4,26 @@ public class CalcularIva {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		boolean seguir = false;
+		
 		System.out.println("Calculadora IVA 21%");
 		System.out.println("------------------------");
 		
-		System.out.println("Por favor dame el importe en €");
+		do {
+			System.out.println("Por favor dame el importe en €");
+			float importe = sc.nextFloat();		
+			System.out.println( "iva repercutido: " + importe * 0.21 );
+			System.out.println("¿ Quieres calcular de nuevo ?  (S/N)");
+			seguir = "s".equalsIgnoreCase(sc.next());
+		}while( seguir );
 		
-		Scanner sc = new Scanner(System.in);		
-		float importe = sc.nextFloat();
 		
-		System.out.println( "iva repercutido: " + importe * 0.21 );
+		System.out.println("Agur Venur");
 		
-
+		
+		sc.close();
+		
 	}
 
 }
