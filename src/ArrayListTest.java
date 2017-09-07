d2a0afaec69439026a1fdb6bfb5ca43b4bebc236
anderuraga <ander.uraga@gmail.com>
2017-09-07 13:15:42 +0200

    Test para manejo basico de ArrayList

---
+++ src/ArrayListTest.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class ArrayListTest extends TestCase {
+
+	
+	static final String FRANCIA = "Francia";
+	static final String JAPON   = "Japon";
+	static final String ITALIA  = "Italia";
+	static final String NORUEGA = "Noruega";
+	
+	public void testPaises() {
+		
+		ArrayList paises = new ArrayList();
+		
+		assertEquals(0 , paises.size() );
+		
+		paises.add(FRANCIA);
+		assertEquals(1 , paises.size() );
+		
+		paises.add(JAPON);
+		paises.add(ITALIA);
+		paises.add(NORUEGA);
+		assertEquals(4 , paises.size() );
+		
+		paises.remove(0);
+		assertEquals(3 , paises.size() );
+		assertEquals( JAPON , paises.get(0) );
+		
+		
+		
+	}
+	
+}
