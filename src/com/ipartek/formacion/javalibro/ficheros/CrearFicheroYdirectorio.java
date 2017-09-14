e0e55fb2726e7271fd147dcc181012016b64edd3
anderuraga <ander.uraga@gmail.com>
2017-09-14 12:49:53 +0200

    otro ejercicio ficheros

---
+++ src/com/ipartek/formacion/javalibro/ficheros/CrearFicheroYdirectorio.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.javalibro.ficheros;
+
+/**
+ * Pide por pantalla: 
+ *    <ol>
+ *        <li>Nombre de un directorio</li>
+ *        <li>Nombre fichero.txt</li>
+ *        <li>Palabras a guardar dentro del fichero.txt</li>
+ *    </ol>    
+ *    
+ * Crea la carpeta en C:   
+ *    
+ * @author ur00
+ *
+ */
+public class CrearFicheroYdirectorio {
+
+}
