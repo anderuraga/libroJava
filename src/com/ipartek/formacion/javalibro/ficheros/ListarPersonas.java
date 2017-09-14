089d40db2ed8f27baabec2cc95aca7c2024c1402
anderuraga <ander.uraga@gmail.com>
2017-09-14 12:53:33 +0200

    mas ejercicios

---
+++ src/com/ipartek/formacion/javalibro/ficheros/ListarPersonas.java
@@ -0,0 +1,18 @@
+package com.ipartek.formacion.javalibro.ficheros;
+
+/**
+ * Leer linea a linea el fichero "data/personas.txt" y mostrar por pantalla.<br>
+ * Contar el total de personas leidas.
+ * 
+ * 
+ * @author ur00
+ *
+ */
+public class ListarPersonas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
