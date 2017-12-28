60b9d692c4bc869477a43ed83840463c665103a8
anderuraga <ander.uraga@gmail.com>
2017-12-28 11:22:21 +0100

    clase para eliminar

--- src/com/ipartek/formacion/javalibro/pojo/ClaseParaEliminar.java
+++ src/com/ipartek/formacion/javalibro/pojo/ClaseParaEliminar.java
@@ -2,4 +2,16 @@ package com.ipartek.formacion.javalibro.pojo;
 
 public class ClaseParaEliminar {
 
+	public static void main(String[] args) {
+		
+		String nombre = "Eritz";
+		String apellido = "Hurtado";
+		
+		System.out.println("Hola " + nombre  + " " + apellido);
+		
+		
+		System.out.printf("Hola %s %s" , nombre, apellido);
+		
+	}
+	
 }
